4f6c9c78d6e0704f3f2279c3d03f96d7fc8e4b35
Badrinath Parmeshwar Jawale <91363239+Sidheshwarj2001@users.noreply.github.com>
2021-09-25 14:22:54 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,18 @@
+
+
+function First(){
+    return(
+           
+           <div className = "first_div">
+               <h1 >welcome to our website</h1>
+               <li className = "btn">
+                   <button>Home</button>
+                   <button>Contact Us</button>
+               </li>
+            </div>
+            
+      
+    );
+}
+
+export default First;
---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import First from './Component/First';
+import './Component/First.css';
+
+
+ReactDom.render(
+    <div>
+    <First></First>
+    </div>
+    
+
+    ,document.getElementById("root")
+);
